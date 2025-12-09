5c899edbf8dc5670934674d9992d1495c5aaff6a
kinar aurasae <nanamika1230@gmail.com>
2025-12-09 18:07:12 +0700

    tugas

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        return view('categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $category = Category::create(['name' => $request->name]);
+
+        return response()->json([
+            'message' => $category,
+            'data' => $category
+        ],);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $category = Category::findOrFail($id);
+        return response()->json(['data' => $category]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+        $category = Category::findOrFail($id);
+        return view('categories.edit', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate(['name' => 'required|max:255']);
+        $category = Category::findOrFail($id);
+        $category->update(['name' => $request->name]);
+
+        return response()->json([
+            'message' => 'Category updated successfully',
+            'data' => $category
+        ],200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return response()->json([
+            'message' => 'Category deleted successfully'
+        ],200);
+    }
+}
